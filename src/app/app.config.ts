c06d3d3abcf56ca24c1a907e63c27b03daa2e774
Irene V. <irene.v1995@gmail.com>
2025-04-23 17:29:06 +0200

    Cosas varias de configuración del front

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { baseUrlInterceptor } from './shared/interceptors/base-url.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withPreloading(PreloadAllModules)
+    ),
+    provideHttpClient(withInterceptors([baseUrlInterceptor])),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadChildren: () =>
+            import('./home/home.routes').then((h) => h.homeRoutes),
+    },
+    /* { path: '', redirectTo: '/home/topics', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home/topics' }, */
+];
