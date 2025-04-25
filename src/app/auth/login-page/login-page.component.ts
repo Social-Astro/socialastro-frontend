c1da977170c467886a1ac40411f12e7e79669fcb
Sandra <san.mo.dra5@gmail.com>
2025-04-25 21:50:43 +0200

    feat: implement login and registration forms with validation and styling

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ValidationClassesDirective } from '../../shared/directives/validation-classes.directive';
 
 @Component({
-  selector: 'app-login-page',
-  imports: [],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+    selector: 'login-page',
+    imports: [RouterLink, FormsModule, ReactiveFormsModule, ValidationClassesDirective],
+    templateUrl: './login-page.component.html',
+    styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+    #authService = inject(AuthService);
+    #router = inject(Router);
 
+    loginForm = new FormGroup({
+        username: new FormControl('', {
+            nonNullable: true,
+            validators: [Validators.required]
+        }),
+        password: new FormControl('', {
+            nonNullable: true,
+            validators: [Validators.required]
+        })
+    });
+
+    login() {
+        const newLoggin = {
+            username: this.loginForm.value.username!,
+            password: this.loginForm.value.password!
+        };
+        console.log('Login:', newLoggin);
+        `
+        `;
+        if (this.loginForm.valid) {
+            this.#authService.login(newLoggin).subscribe({
+                next: () => {
+                    this.saved = true;
+                    console.log('%cLogin exitoso', 'color: green;');
+                    this.#router.navigate(['/home']);
+                },
+                error: (err) => console.error('Login error:', err)
+            });
+        } else {
+            console.error('Formulario inválido');
+        }
+    }
+
+    private saved = false;
+
+    resetForm() {
+        this.loginForm.reset();
+        this.saved = false;
+    }
+
+    canDeactivate() {
+        return this.saved || this.loginForm.pristine || confirm('¿De verdad quieres salir? Vas a perder los datos.');
+    }
 }
--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -4,26 +4,27 @@ export interface User {
     password?: string;
     name: string;
     email: string;
-    avatar: string;
-    header: string;
-    bio: string;
+    avatar?: string;
+    header?: string;
+    bio?: string;
     createdAt: Date;
-    friend_ids: number[];
+    updatedAt?: Date;
+    friend_ids?: number[];
+    role: string;
 }
 
 export interface UserLogin {
-    email: string;
+    username: string;
     password: string;
 }
 
 export interface UserProfileEdit {
-    name: string;
-    email: string;
-    avatar: string;
-    header: string;
-    bio: string;
-    createdAt: Date;
-    friend_ids: number[];
+    name?: string;
+    email?: string;
+    avatar?: string;
+    header?: string;
+    bio?: string;
+    updatedAt: Date;
 }
 
 export interface UserPasswordEdit {
