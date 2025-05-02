df89edc0daccda4215c101c83b4d49c1f9f1a0e9
Sandra <san.mo.dra5@gmail.com>
2025-05-02 20:42:33 +0200

    Update home.routes

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -4,20 +4,15 @@ export const homeRoutes: Routes = [
     {
         path: 'topics',
         loadChildren: () => import('./topics/topics.routes').then((r) => r.topicsRoutes),
-        title: 'Temas * Social Astro'
-        loadChildren: () =>
-            import('./topics/topics.routes').then((r) => r.topicsRoutes),
         title: 'Temas ** Social Astro'
     },
     {
         path: 'sections',
-        loadChildren: () =>
-            import('./sections/section.routes').then((s) => s.sectionRoutes),
+        loadChildren: () => import('./sections/section.routes').then((s) => s.sectionRoutes)
     },
     {
         path: 'posts',
-        loadChildren: () =>
-            import('./posts/posts.routes').then((p) => p.postsRoutes),
+        loadChildren: () => import('./posts/posts.routes').then((p) => p.postsRoutes)
     },
     { path: '', redirectTo: '/home/topics', pathMatch: 'full' }
 ];
