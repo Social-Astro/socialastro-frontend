180652cd2ea775a115e4de41b9810a789c16bcc3
Irene V. <irene.v1995@gmail.com>
2025-05-03 20:20:52 +0200

    fixed some issues after merge

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { TopMenuComponent } from './app/top-menu/top-menu.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { AppComponent } from './app/app.component';
 
-export const appConfig: ApplicationConfig = {
+// Comento esto para que no se rompa
+/* export const appConfig: ApplicationConfig = {
     providers: [
+        provideExperimentalZonelessChangeDetection(),
         provideHttpClient(withFetch()),
         provideAnimationsAsync(),
         providePrimeNG({
             theme: { preset: Aura, options: { darkModeSelector: '.p-dark' } }
         })
     ]
-};
+}; */
 
-bootstrapApplication(TopMenuComponent, appConfig).catch((err) => console.error(err));
+// Dejo esto así para que no se rompa, luego lo volvemos a cambiar a lo que tenías tú AppComponent > TopMenuComponent
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
