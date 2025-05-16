e1e1c13632cae074a15cc8c41e4b11ed8664f3ef
Sandra <san.mo.dra5@gmail.com>
2025-05-16 20:59:52 +0200

    .

--- src/app/shared/interceptors/base-url.interceptor.ts
+++ src/app/shared/interceptors/base-url.interceptor.ts
@@ -1,9 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
-    /* const serverUrl = process.env['URL_BACKEND']; */
-    /* const serverUrl = 'https://vps-94333546.vps.ovh.net/socialastro-backend'; */
-    const serverUrl = 'http://localhost:3000';
+    // const serverUrl = 'http://localhost:3000';
+    const serverUrl = 'http://vps-94333546.vps.ovh.net:8080/socialastroapi';
     const clonedReq = req.clone({
         url: `${serverUrl}/${req.url}`
     });
