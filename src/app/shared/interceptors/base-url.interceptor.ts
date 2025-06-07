cd869d46980ce50121f86e6b3a20061c9ed00f89
Sandra <san.mo.dra5@gmail.com>
2025-06-07 10:36:13 +0200

    pppp

--- src/app/shared/interceptors/base-url.interceptor.ts
+++ src/app/shared/interceptors/base-url.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
-    const serverUrl = isDevMode() ? 'http://localhost:3000/' : 'https://vps-94333546.vps.ovh.net:8080/socialastroapi';
+    const serverUrl = isDevMode() ? 'http://localhost:3000/' : 'https://api.socialastro.es/';
     const clonedReq = req.clone({
         url: `${serverUrl.replace(/\/$/, '')}/${req.url.replace(/^\//, '')}`
     });
