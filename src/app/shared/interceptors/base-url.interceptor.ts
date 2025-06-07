2fd7f1504b4f413715c575b678a5b645647e5e7b
Sandra <san.mo.dra5@gmail.com>
2025-06-07 10:33:16 +0200

    pppp

--- src/app/shared/interceptors/base-url.interceptor.ts
+++ src/app/shared/interceptors/base-url.interceptor.ts
@@ -1,8 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { isDevMode } from '@angular/core';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
-    const serverUrl = environment.apiBaseUrl;
+    const serverUrl = isDevMode() ? 'http://localhost:3000/' : 'https://vps-94333546.vps.ovh.net:8080/socialastroapi';
     const clonedReq = req.clone({
         url: `${serverUrl.replace(/\/$/, '')}/${req.url.replace(/^\//, '')}`
     });
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-import { isDevMode } from '@angular/core';
-
-export const environment = {
-    production: !isDevMode(),
-    apiBaseUrl: isDevMode() ? 'http://localhost:3000/' : 'https://vps-94333546.vps.ovh.net:8080/socialastroapi'
-};
